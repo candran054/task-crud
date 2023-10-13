5aad74ea30fb29b018132e78a6071e0ab337a58e
candran054 <candran054@gmail.com>
2023-10-13 11:57:10 +0700

    add logout function

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -5,15 +5,42 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggleSidebar } from "../redux/sidebarSlice";
 import { RootState } from "../redux/store";
 import Image from "next/image";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+  const { push } = useRouter();
+
   const dispatch = useDispatch();
   const isSidebarVisible = useSelector(
     (state: RootState) => state.sidebar.isSidebarVisible,
   );
 
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleSubmenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   const handleSidebarToggle = () => {
     dispatch(toggleSidebar(!isSidebarVisible));
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/auth/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      push("/");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -22,7 +49,10 @@ export default function Header() {
         onClick={handleSidebarToggle}
         className={`text-xl text-indigo-500 md:hidden`}
       />
-      <div className="flex pr-4 justify-end">
+      <div
+        onClick={toggleSubmenu}
+        className="flex relative flex-col pr-9 justify-end cursor-pointer"
+      >
         <Image
           src="/images/Me.jpg"
           width={50}
@@ -36,6 +66,32 @@ export default function Header() {
             backgroundColor: "ivory",
           }}
         />
+        <AnimatePresence>
+          {showMenu && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  type: "tween",
+                  duration: 0.2,
+                },
+              }}
+              exit={{
+                opacity: 0,
+                transition: {
+                  type: "tween",
+                  duration: 0.2,
+                },
+              }}
+              onClick={handleSubmit}
+              className="absolute flex justify-center items-center rounded-md top-10 right-2 w-20 px-4 py-[0.15rem]
+            bg-zinc-200"
+            >
+              <p className="text-black text-sm font-semibold">Logout</p>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
